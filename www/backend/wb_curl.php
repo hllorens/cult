80c87facaccd4fac1fd63121ceabb16743cb3239
hector llorens <hectorlm1983@gmail.com>
2019-03-10 23:19:08 +0100

    more

--- www/backend/wb_curl.php
+++ www/backend/wb_curl.php
@@ -23,13 +23,15 @@ function get_wb($indicator,$country,$debug=false){
 
 	$curl_data=json_decode($response,true);
 	
-	if($curl_data==null || count($curl_data)==0){
-		// handle late log
+	if($curl_data==null || count($curl_data)==0 || count($curl_data)==1){
+		$latelog = fopen("late_wb.log", "a") or die("Unable to open/create late_wb.log!");
+		fwrite($latelog, date('Y-m-d H:i:s')."  wb_curl.php. Error empty curl $url_and_query<br />\n");
+		fclose($latelog);
 		return $old;
 	}
 	
 	//var_dump($curl_data);
-	
+	$new_report="";
 	for($i=0;$i<count($curl_data[1]);$i++){
 		if($curl_data[1][$i]['value']!=null && trim($curl_data[1][$i]['value'])!=''){
 			$reduction_factor=1; // as-is
@@ -45,7 +47,13 @@ function get_wb($indicator,$country,$debug=false){
 				$decimals=4;
 			}
 			$val=toFixed((double)$curl_data[1][$i]['value']/$reduction_factor, $decimals, "wb val");
-			echo $curl_data[1][$i]['date']." ".$curl_data[1][$i]['country']['value']." (".$val.")<br />"; //
+			if($val==null || empty($val) || $val==0){
+				$latelog = fopen("late_wb.log", "a") or die("Unable to open/create late_wb.log!");
+				fwrite($latelog, date('Y-m-d H:i:s')."  wb_curl.php. Error 0 value curl $url_and_query<br />".$curl_data[1][$i]['date']." $indicator $country $val<br />\n");
+				fclose($latelog);
+				continue;
+			}
+			if($debug) echo $curl_data[1][$i]['date']." ".$curl_data[1][$i]['country']['value']." (".$val.")"; //
 			if(!array_key_exists($indicator,$old)){
 				$old[$indicator]=array();
 			}
@@ -55,22 +63,29 @@ function get_wb($indicator,$country,$debug=false){
 			if(array_key_exists($curl_data[1][$i]['date'],$old[$indicator][$country])){
 				if($old[$indicator][$country][$curl_data[1][$i]['date']]!=$val){
 					//late log change past
+				}else{
+					if($debug) echo "no update";
 				}
 			}else{
+				// avoid crazy email
+				$new_report.="$url_and_query<br /> new val ".$curl_data[1][$i]['date']." ".$curl_data[1][$i]['country']['value']." (".$val.")<br /><br />";
+				echo "new val ".$curl_data[1][$i]['date']." ".$curl_data[1][$i]['country']['value']." (".$val.")<br />";
 				$old[$indicator][$country][$curl_data[1][$i]['date']]=$val;
 				// no.. json exportable to csv is fine, no add it to firebase
 			}
+			if($debug) echo "<br />";
 		}
 	}
+	if($new_report!=""){
+		send_mail("wb new $indicator $country","$new_report<br />","hectorlm1983@gmail.com");
+	}
 	
-	/*
-		if($new_period_report!="" && !$first_time_financials){
-			echo 'new financials '.$name,"$url_and_query<br />In ".$symbol." ".$new_period_report."<br /><br />";
-			//send_mail('new financials '.$name,"$url_and_query<br />In ".$symbol." ".$new_period_report."<br /><br />","hectorlm1983@gmail.com");
-		}
-		if($change_past_report!=""){
-			send_mail('financials change past '.$name,"$url_and_query<br />In ".$symbol." ".$change_past_report."<br /><br />","hectorlm1983@gmail.com");
-		}*/
+
+	echo date('Y-m-d H:i:s')." updating $out_json\n";
+	$old_str=json_encode( $old );
+	$json_file = fopen($out_json, "w") or die("Unable to open file $out_json!");
+	fwrite($json_file, $old_str);
+	fclose($json_file);
 
     ksort($old);
     return $old;
@@ -84,7 +99,7 @@ if( isset($_REQUEST['indicator']) ){
     $debug=false;
     if( isset($_REQUEST['debug']) && ($_REQUEST['debug']=="true" || $_REQUEST['debug']=="1")){
         $debug=true;
-    }
+    } 
     echo "manual mode<br /><br />";
     $curl_data=get_wb($_REQUEST['indicator'],$country,$debug);
     echo "Result:<br />---<pre>".json_encode($curl_data, JSON_PRETTY_PRINT)."</pre>---<br />";
