6d54af62c4194e2ebd7604bd90cd861df9f378dc
hector llorens <hectorlm1983@gmail.com>
2019-10-18 00:21:13 +0200

    funds

--- www/backend/get_funds.php
+++ www/backend/get_funds.php
@@ -11,21 +11,58 @@ require_once("email_config.php");
 $list="sabadell-prudente-base-fi";
 $list="$list,amundi-index-msci-europe-aec,amundi-msci-wrld-ae-c,amundi-index-sp-500-aec";
 
+$list_details["amundi-index-sp-500-aec"]=array(
+	"isin"=> "LU0996179007",
+	"morningstar-id"=> "F00000T7PZ",
+	"high"=> "-",
+	"low"=> "-"
+);
+$list_details["amundi-index-sp-500-aec"]=array(
+	"isin"=> "LU0996179007",
+	"morningstar-id"=> "F00000T7PZ",
+	"high"=> "-",
+	"low"=> "-"
+);
+$list_details["amundi-msci-wrld-ae-c"]=array(
+	"isin"=> "LU0996182563",
+	"morningstar-id"=> "F00000T66U",
+	"high"=> "-",
+	"low"=> "-"
+);
+$list_details["amundi-index-sp-500-aec"]=array(
+	"isin"=> "LU0996179007",
+	"morningstar-id"=> "F00000T7PZ",
+	"high"=> "-",
+	"low"=> "-"
+);
 
 echo date('Y-m-d H:i:s')." starting<br />";
 
-$timestamp_date=date("Y-m-d");
+$timestamp_date=date("Y-m-d H:i:s");
+
+$cookieFile = "cookies.txt";
+if(!file_exists($cookieFile)) {
+    $fh = fopen($cookieFile, "w");
+    fwrite($fh, "");
+    fclose($fh);
+}
 
 function get_details($symbol,$debug=false){
 	$details=array();
 	$the_url="https://www.investing.com/funds/";
-	$the_url="https://es.investing.com/funds/";
+	//$the_url="https://es.investing.com/funds/"; // probar esta version
     $url_and_query=$the_url.$symbol;
-	$url_and_query="https://www.amundi.es/retail/product/view/LU0996179007";
+	//$url_and_query="https://www.finect.com/fondos-inversion/LU0996179007-Amundi_is_sp_500_aec"; //alt
+	//$url_and_query="https://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F00000T7PZ"; // sino esta
+	//$url_and_query="https://www.amundi.es/retail/product/view/LU0996179007"; // esta ya he comprobado q sí que va...
     echo "stock $url_and_query<br />";
     $curl = curl_init();
     curl_setopt( $curl, CURLOPT_URL, $url_and_query );
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'); // help needed for investing
+    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true ); // save in variable, no stdout
+	//curl_setopt($curl, CURLOPT_REFERER, 'http://www.cognitionis.com/'); // not needed yet
+	//curl_setopt($curl, CURLOPT_COOKIEFILE, $cookieFile); // Cookie aware not needed yet
+	//curl_setopt($curl, CURLOPT_COOKIEJAR, $cookieFile); // Cookie aware not needed yet
     $response = curl_exec( $curl ); //utf8_decode( not necessary
     curl_close( $curl );
     preg_match("/^.*moved to <a href=\".*stockdetails\/([^\"]*)\">.*$/m", $response, $redirect);
@@ -45,21 +82,25 @@ function get_details($symbol,$debug=false){
     $response=preg_replace("/<\/(td|table)>/", "\n", $response);
     if($debug) echo "aaa.<pre>".htmlspecialchars($response)."</pre>";
 
-    preg_match("/if=\"quotes_summary_current_data\"[^>]*>\s*([^<]*)</m", $response, $value);
+	// investing quotes_summary_current_data and id="last_last"
+    preg_match("/id=\"last_last\"[^>]*>\s*([^<]*)</m", $response, $value);
     if(count($value)<2){
         echo "<br />Empty value skipping, email sent...<br />";
-        //send_mail('Bad crawl '.$symbol,'<br />Empty value, skipping...<pre>'.htmlspecialchars($response).'</pre><br /><br />',"hectorlm1983@gmail.com");
+        send_mail('Bad crawl '.$symbol,'<br />Empty value, skipping...<pre>'.htmlspecialchars($response).'</pre><br /><br />',"hectorlm1983@gmail.com");
         return $details;
     }
     $value=str_replace(",","",trim($value[1]));
     if(!isset($value) || $value=="" || $value=="-"){
         echo "<br />Empty value skipping, email sent...<br />";
-        //send_mail('Error '.$symbol,'<br />Empty value(!isset, "" or "-"), skipping...<pre>'.htmlspecialchars($response).'</pre><br /><br />',"hectorlm1983@gmail.com");
+        send_mail('Error '.$symbol,'<br />Empty value(!isset, "" or "-"), skipping...<pre>'.htmlspecialchars($response).'</pre><br /><br />',"hectorlm1983@gmail.com");
         return $details;
     }
-    if($debug) echo "value: (".$value.")<br />";
-/*
-    preg_match("/data-role=\"percentchange\"[^>]*>\s*([^<]*)</m", $response, $changep);
+    //if($debug) 
+		echo "value: (".$value.")<br />";
+
+
+	// TODO: we could ignore this and calculate from day before...
+    preg_match("/id=\"fl_header_pair_pch\"[^>]*>\s*([^<]*)</m", $response, $changep);
     if(count($changep)<2){
         echo "<br />Empty changep skipping, email sent...<br />";
         send_mail('Error '.$symbol,'<br />Empty changep, skipping...<br /><br />',"hectorlm1983@gmail.com");
@@ -68,46 +109,15 @@ function get_details($symbol,$debug=false){
     $changep=$changep[1];
     $changep=str_replace("%","",trim($changep));
     $changep=str_replace("+","",trim($changep));
-    if($debug) echo "change: (".$changep.")<br />";
-*/
-    /*preg_match("/Expenses<[^<]*<[^<]*<[^<]*<p [^>]*>\s*([^<]*)\s*</m", $response, $expenses);
-    if(count($expenses)>1){
-        $expenses=str_replace(",","",trim($expenses[1]));
-        if($debug) echo "expenses: (".$expenses.")<br />";
-    }else{
-        echo "<br />Empty range_52week skipping, email sent...<br />";
-        send_mail('Error '.$symbol,'<br />Empty range_52week, skipping...<br /><br />',"hectorlm1983@gmail.com");
-        return $details;
-    }*/
-
-    $mktcap=0;
-    $details['value']=$value;
-    $details['session_change_percentage']=0;
-    $details['expenses']=0;
-
-/*		$mktcap_updated=false;
-        preg_match("/>\s*Market Cap[^<]*<[^<]*<[^<]*<[^<]*<p [^>]*>\s*([^<]*)\s*</m", $response, $mktcap);
-        if(count($mktcap)>1){
-            $mktcap=trim($mktcap[1]);
-            if($mktcap=="-" || $mktcap==""){
-				$mktcap=0;
-            }else{
-				$mktcap=format_billions($mktcap);
-				$mktcap_updated=true;
-			}
-        }else{
-			echo "<br />Empty mktcap skipping, email sent if old...<br />";
-			$mktcap=0;
-        }
-        
-		if($mktcap_updated){
-			// alert special
-		}
-    }*/
+    //if($debug) 
+		echo "change: (".$changep.")<br />";
 
 
-    // assignment to the array, will be 0 if went wrong
-    //$details['mktcap']=$mktcap;
+
+    $details['v']=$value;
+    $details['c']=$changep;
+	//$details['date']=date("Y-m-d H:i:s");
+
 	
 	return $details;
 }
@@ -149,10 +159,53 @@ if(isset($_REQUEST['symbol'])){
 	$stock_last_detail_updated_f = fopen("stock_last_detail_updated.txt", "w") or die("Unable to open file!");
 	fwrite($stock_last_detail_updated_f, $stock_last_detail_updated);
 	fclose($stock_last_detail_updated_f);*/
+	$funds_arr=array(); 
+	if(file_exists ( 'funds.json' )){
+		echo "funds.json exists -> reading...<br />";
+		$funds_arr = json_decode(file_get_contents('funds.json'), true);
+	}else{
+		echo "<br/><br/><span style=\"color:red\">ERROR:</span>funds.json does NOT exist -> using an empty array<br />";
+	}
+
 	echo "<br />under construction...<br />";
+
 	$the_url_query_arr = explode(",", $list);
-	get_details($the_url_query_arr[0],$debug);
+	
+	foreach($the_url_query_arr as $key){
+		$temp_result=get_details($key,$debug);
+		if(!empty($temp_result)){
+			if(!array_key_exists($key,$funds_arr)) $funds_arr[$key]=array();
+			$funds_arr[$key]['v']=$temp_result['v'];
+			$funds_arr[$key]['c']=$temp_result['c'];
+			$funds_arr[$key][substr($timestamp_date,0,10)]=$temp_result;
+		}else{
+			break;
+		}
+		// to avoid server ban
+		sleep(0.30);
+	}
+
+	$funds_arr_json_str=json_encode( $funds_arr );
+
+	// update funds.json
+	echo date('Y-m-d H:i:s')." updating json\n";
+	$json_file = fopen("funds.json", "w") or die("Unable to open file funds.json!");
+	fwrite($json_file, $funds_arr_json_str);
+	fclose($json_file);
+
+	// backup history (monthly)
+	if(!file_exists( date("Y-m").'.funds.json' )){
+		echo "creating backup: ".date("Y-m").".funds.json<br />";
+		$json_fileb = fopen(date("Y-m").".funds.json", "w") or die("Unable to open file funds.json!");
+		fwrite($json_fileb, $funds_arr_json_str);
+		fclose($json_fileb);
+	}
+
+
 }
+
+
+
 
 echo date('Y-m-d H:i:s')." ending<br />";
 
